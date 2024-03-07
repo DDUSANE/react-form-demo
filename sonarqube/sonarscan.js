4cff7d3dff16be6deba6db142a5cd33b4191e7e1
ddusane <63367839+DDUSANE@users.noreply.github.com>
2024-03-07 11:47:18 +0530

    Update sonarscan.js
    Signed-off-by: ddusane <63367839+DDUSANE@users.noreply.github.com>

--- sonarqube/sonarscan.js
+++ sonarqube/sonarscan.js
@@ -4,7 +4,7 @@ const scanner = require('sonarqube-scanner')
 scanner(
     {
         serverUrl: 'http://localhost:9000',
-        token: "sqa_a49d4cb095d040fcf138ff32db3c64df9ef03d89",
+        token: "squ_7dbba0b52bc389c22f205094dede4ef08d909d02",
         options: {
             'sonar.projectKey': 'evs-react-project1',
             'sonar.projectName': 'evs-react-project1',
